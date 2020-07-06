bb9f5392a5b3d6fd18b8e53ca5d915dbd901a211
luzdegea <19971997a-A>
2020-07-06 16:36:51 +0200

    aggiunto json

---
+++ Telegram Bot/Food_Helper/.idea/Test.py
@@ -0,0 +1,9 @@
+import os, io
+from google.cloud import vision
+from google.cloud.vision import types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'foodhelper-sysag-356f63fafc5e.json'
+
+client = vision.ImageAnnotatorClient()
+
+
