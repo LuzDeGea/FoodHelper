4ae3e474524ec43e7879b160c6d62ecec8fb8e27
Antonio Francesco Fiore <antoniofrancescofiore98@gmail.com>
2020-07-07 15:10:28 +0200

    rimossa cartella inutile

--- Telegram Bot/Food_Helper/.idea/Test.py
+++
@@ -1,9 +0,0 @@
-import os, io
-from google.cloud import vision
-from google.cloud.vision import types
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'foodhelper-sysag-356f63fafc5e.json'
-
-client = vision.ImageAnnotatorClient()
-
-
